bb19a30668e1e3159f05a902d376a3d5f28fb956
Benjamim <benjamim@mac-de-benjamim.local>
2019-05-28 20:36:03 -0700

    references done

--- iOS_CoreData/ViewController.swift
+++ iOS_CoreData/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Properties
+    //MARK: TextFields
+    @IBOutlet weak var addPais: UITextField!
+    @IBOutlet weak var addCidade: UITextField!
+    @IBOutlet weak var especPais: UITextField!
+    @IBOutlet weak var cidade: UITextField!
+    @IBOutlet weak var pais: UITextField!
+    @IBOutlet weak var paisAntigo: UITextField!
+    @IBOutlet weak var paisNovo: UITextField!
+    //MARK: Labels
+    @IBOutlet weak var paisCidade: UILabel!
+    @IBOutlet weak var cidadePais: UITextField!
+    @IBOutlet weak var final: UILabel!
+    //MARK: Actions
+    
+    @IBAction func adicionarPais(_ sender: Any) {
+    }
+    @IBAction func removerPais(_ sender: Any) {
+    }
+    @IBAction func adicionarCidade(_ sender: Any) {
+    }
+    @IBAction func bPaisCidade(_ sender: Any) {
+    }
+    @IBAction func bCidadePais(_ sender: Any) {
+    }
+    @IBAction func atualizar(_ sender: Any) {
+    }
+    @IBAction func todosPaises(_ sender: Any) {
+    }
+    @IBAction func todasCidades(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
